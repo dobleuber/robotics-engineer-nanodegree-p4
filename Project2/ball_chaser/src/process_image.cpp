8d32a5c1193b1b91178ed8c1c5cb06a855b8a953
dobleuber <wbertc@gmail.com>
2021-07-19 09:20:32 -0700

    Fix Review issues

--- Project2/ball_chaser/src/process_image.cpp
+++ Project2/ball_chaser/src/process_image.cpp
@@ -34,11 +34,12 @@ void process_image_callback(const sensor_msgs::Image img) {
     int ballPosition = -1;
     bool ballFound = false;
 
-    for (int i = 0; i < img.height * img.step; i++) {
+    for (int i = 0; i < img.height * img.step; i+=3) {
         int pixel_x = i % img.step;
-        if (img.data[i] == white_pixel) {
+        if (img.data[i] == white_pixel && img.data[i+1] == white_pixel && img.data[i+2] == white_pixel) {
             ballFound = true;
             ballPosition = pixel_x;
+            break;
         }
     }
 
